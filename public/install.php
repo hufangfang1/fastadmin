8e722656f21336caf33479c60757a4883f148f82
胡方方 <851118072@qq.com>
2024-03-22 10:42:44 +0800

    debuge

--- public/install.php
+++ public/install.php
@@ -13,7 +13,6 @@
 // 建议安装完成后移除此文件
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
- var_dump(__DIR__ . '/../thinkphp/base.php');
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/base.php';
 
