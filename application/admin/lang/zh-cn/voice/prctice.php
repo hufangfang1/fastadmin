cea10790888c06fcf7b4c48638c2b5217b56d9f3
hufangfang1 <851118072@qq.com>
2024-03-22 05:33:46 +0000

    debuge

---
+++ application/admin/lang/zh-cn/voice/prctice.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'File_path_image'  => '文件',
+    'File_id'          => '文件 ID',
+    'Task_id'          => '任务 ID',
+    'Finetuned_output' => '模型名称',
+    'Status'           => '状态',
+    'Status -1'        => '失败',
+    'Set status to -1' => '设为失败',
+    'Status 0'         => '进行中',
+    'Set status to 0'  => '设为进行中',
+    'Status 1'         => '完成',
+    'Set status to 1'  => '设为完成'
+];
---
+++ public/assets/js/backend/voice/prctice.js
@@ -0,0 +1,57 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'voice/prctice/index' + location.search,
+                    add_url: 'voice/prctice/add',
+                    edit_url: 'voice/prctice/edit',
+                    del_url: 'voice/prctice/del',
+                    multi_url: 'voice/prctice/multi',
+                    import_url: 'voice/prctice/import',
+                    table: 'voice_prctice',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'file_path_image', title: __('File_path_image'), operate: false, events: Table.api.events.image, formatter: Table.api.formatter.image},
+                        {field: 'file_id', title: __('File_id'), operate: 'LIKE'},
+                        {field: 'task_id', title: __('Task_id'), operate: 'LIKE'},
+                        {field: 'finetuned_output', title: __('Finetuned_output'), operate: 'LIKE'},
+                        {field: 'status', title: __('Status'), searchList: {"-1":__('Status -1'),"0":__('Status 0'),"1":__('Status 1')}, formatter: Table.api.formatter.status},
+                        {field: 'update_time', title: __('Update_time'), operate:'RANGE', addclass:'datetimerange', autocomplete:false, formatter: Table.api.formatter.datetime},
+                        {field: 'create_time', title: __('Create_time'), operate:'RANGE', addclass:'datetimerange', autocomplete:false, formatter: Table.api.formatter.datetime},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
